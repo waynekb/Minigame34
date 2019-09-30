1a7dca17d616a26528d47991b5642e9932ed1a5f
waynekb <342818240@qq.com>
2019-09-30 09:36:05 +0800

    串联场景

--- Assets/Scripts/loading_scene/ChangeScene.cs
+++ Assets/Scripts/loading_scene/ChangeScene.cs
@@ -24,38 +24,61 @@ public class ChangeScene : MonoBehaviour
                 }
                 break;
             case "Second":
-                PassSceneName.SceneName = "Third";
+                PassSceneName.SceneName = "Third_1";
                 if (Level.level < 2)
                 {
                     Level.level = 2;
                 }
                 break;
-            case "Third":
-                PassSceneName.SceneName = "Fouth";
+            case "Third_1":
+                PassSceneName.SceneName = "TuZi";
+                break;
+            case "TuZi":
+                PassSceneName.SceneName = "Niu_Dan";
+                break;
+            case "Niu_Dan":
+                PassSceneName.SceneName = "Fourth";
                 if (Level.level < 3)
                 {
                     Level.level = 3;
                 }
                 break;
-            case "Fouth":
-                PassSceneName.SceneName = "Fifth";
+            case "Fourth":
+                PassSceneName.SceneName = "Third_2";
+                break;
+            case "Third_2":
+                PassSceneName.SceneName = "JinRu";
                 if (Level.level < 4)
                 {
                     Level.level = 4;
                 }
                 break;
+            case "JinRu":
+                PassSceneName.SceneName = "Fifth";
+                break;
             case "Fifth":
-                PassSceneName.SceneName = "Start";
+                PassSceneName.SceneName = "Sixth";
                 if (Level.level < 5)
                 {
                     Level.level = 5;
                 }
+                break;
+            case "Sixth":
+                PassSceneName.SceneName = "Start";
                 break;
             default:
                 break;
         }
         Level.Save_level();
         SceneManager.LoadScene("loading_sce");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            next_level();
+        }
     }
 
 }
