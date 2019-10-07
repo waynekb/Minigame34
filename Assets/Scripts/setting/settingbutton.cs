f2e3471b7a3671d22509d4603855b20df23ed2f3
waynekb <342818240@qq.com>
2019-10-07 14:44:33 +0800

    增加弹幕素材

---
+++ Assets/Scripts/setting/settingbutton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class settingbutton : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        GameObject setbtn_obj = GameObject.Find("Canvas/ButtonSetting");
+        Button setbtn = (Button)setbtn_obj.GetComponent<Button>();
+        setbtn.onClick.AddListener(ret_start);
+
+    }
+
+    void ret_start()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
